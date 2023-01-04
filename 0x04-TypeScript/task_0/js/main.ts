8c9bb84df8e3c35f644b0c9c06dbba8905f3ff3b
Kelvin Mwenda <NKMKE1964@gmail.com>
2023-01-04 19:42:27 +0000

    task 1

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,34 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+let student1: Student = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 21,
+  location: "New York"
+};
+
+let student2: Student = {
+  firstName: "Jane",
+  lastName: "Doe",
+  age: 22,
+  location: "Chicago"
+};
+
+let studentsList: Student[] = [student1, student2];
+
+let table = document.createElement("table");
+
+for (let i = 0; i < studentsList.length; i++) {
+  let row = table.insertRow();
+  let firstNameCell = row.insertCell();
+  let locationCell = row.insertCell();
+  firstNameCell.innerHTML = studentsList[i].firstName;
+  locationCell.innerHTML = studentsList[i].location;
+}
+
+document.body.appendChild(table);
